190165bf181b9bd39047927c39a9dbaa1db3c6b1
Axel Vergara <axel.vergara94@gmail.com>
2024-08-30 04:02:47 -0400

    test

--- src/app/counter/components/counter/counter.component.ts
+++ src/app/counter/components/counter/counter.component.ts
@@ -15,11 +15,11 @@ export class CounterComponent{
     public counter: number = 10;
 
     increaseBy(value: number):void {
-    this.counter +=value;
+        this.counter +=value;
     }
 
     resetCounter():void {
-    this.counter = 10;
+        this.counter = 10;
     }
 
 }
